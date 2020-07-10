04c34d9a37e3a4c862e5f5618d339bbfa2d94ae2
X-Green <2271174177@qq.com>
2020-07-10 23:27:08 +0800

    init

---
+++ src/main/java/dev/eeasee/scenemasker/events/OnInit.java
@@ -0,0 +1,10 @@
+package dev.eeasee.scenemasker.events;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OnInit implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Scene masker launched! Made by eeasee");
+    }
+}
