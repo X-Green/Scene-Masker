597abdb04767ae4a03de7e94b68914d833e85fbb
X-Green <2271174177@qq.com>
2020-07-31 12:15:24 +0800

    World

--- src/main/java/dev/eeasee/scenemasker/mixin/ChunkRenderRegionMixin.java
+++ src/main/java/dev/eeasee/scenemasker/mixin/ChunkRenderRegionMixin.java
@@ -1,8 +1,29 @@
 package dev.eeasee.scenemasker.mixin;
 
+import dev.eeasee.scenemasker.utils.MaskerWorldUtils;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChunkRendererRegion.class)
-public class ChunkRenderRegionMixin {
+public abstract class ChunkRenderRegionMixin implements BlockRenderView {
+
+    @Shadow
+    @Final
+    protected World world;
+
+    @Inject(method = "getBlockState", at = @At("HEAD"), cancellable = true)
+    private void getMaskedBlockState(BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
+        if (MaskerWorldUtils.isBlockRenderedMasked(pos, this.world)) {
+            cir.setReturnValue(MaskerWorldUtils.AIR_BLOCK_STATE);
+        }
+    }
 }
--- src/main/java/dev/eeasee/scenemasker/utils/MaskerWorldUtils.java
+++ src/main/java/dev/eeasee/scenemasker/utils/MaskerWorldUtils.java
@@ -1,10 +1,15 @@
 package dev.eeasee.scenemasker.utils;
 
+import dev.eeasee.scenemasker.fakes.WorldInterface;
 import dev.eeasee.scenemasker.world.MaskedWorld;
-import jdk.nashorn.internal.ir.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class MaskerWorldUtils {
+
+    public final static BlockState AIR_BLOCK_STATE = Blocks.AIR.getDefaultState();
 
     public static boolean isBlockRenderedMasked(BlockPos blockPos, MaskedWorld maskedWorld, MaskProperties properties) {
         if (!properties.isApplied) {
@@ -17,4 +22,8 @@ public class MaskerWorldUtils {
         return (properties.isReverted != flag);
     }
 
+    public static boolean isBlockRenderedMasked(BlockPos blockPos, World world) {
+        WorldInterface worldInterface = (WorldInterface)world;
+        return isBlockRenderedMasked(blockPos, worldInterface.getWorldMasker(), worldInterface.getMaskProperties());
+    }
 }
